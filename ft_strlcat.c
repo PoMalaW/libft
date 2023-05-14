9735b946d98c3929db7e6cddc2b87a250ace6906
PoMalaW <pomalow.gaming@gmail.com>
2023-05-14 04:59:59 +0200

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+    j = 0;
+
+    while(dst[i] < '\0')
+        i++;
+    while (i <= (i + size))
+    {
+        dst[i] = src[j];
+        i++;
+        j++;
+    }
+    dst[i] = '\0';
+    return(i + size);
+}
