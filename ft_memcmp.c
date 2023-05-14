d632deba023296e991f12fd205295f7c5b738a96
PoMalaW <pomalow.gaming@gmail.com>
2023-05-14 02:36:17 +0200

    memcmp fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -2,16 +2,18 @@
 
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    int i;
+    size_t i;
+    int *a;
+    int *b;
 
     i = 0;
+    a = (int *)s1;
+    b = (int *)s2;
 
     while(i <= n)
     {
-        if(s2[i] == s1[i])
+        if(a[i] == b[i])
             i++;
-        else
-            return(i);
     }
-    return(i);
+    return(a - b);
 }
