1563a6dd4123fc15946ee800dcd6cd7e62275096
PoMalaW <pomalow.gaming@gmail.com>
2023-05-14 06:41:08 +0200

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+    void *mem;
+
+    if(nmemb == 0 || size == 0)
+        return(NULL);
+    mem = malloc(nmemb* size);
+    if(mem == NULL)
+        return(NULL);
+    ft_bzero(mem, nmemb*size);
+    return(mem);
+}
