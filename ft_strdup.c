d90062fef6797dbf729c819229d938b281418d75
PoMalaW <pomalow.gaming@gmail.com>
2023-05-15 10:48:56 +0200

    strdup +norm update

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,10 +1,24 @@
-char *strdup(const char *s)
+#include "libft.h"
+
+char    *ft_strdup(const char *s)
 {
-    char dest;
-    int i;
+    const void *src;
+    void *dest;
+    size_t n;
 
-    i = ft_strlen(s);
-    i++;
+    n = ft_strlen(s);
+    n++;
 
-    
+    dest = (char *) malloc(n * sizeof(char));
+    src = (const void *)s;
+    ft_memcpy(dest, src, n);
+    return(dest);
+}
+
+int main(void)
+{
+    const char *s;
+
+    s = "W2sZ7kdv2y";
+    printf("%s", strdup(s));
 }
---
+++ main.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char c = ;
+    if (ft_isascii(c))
+    {
+        printf("%c est un caractère ascii\n", c);
+    }
+    else
+    {
+        printf("%c n'est pas un caractère ascii RIP.\n", c);
+    }
+return(0);
+}
