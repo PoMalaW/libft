aa58ae7c18b5916764b5b3c1ab086357ccdcf05b
PoMalaW <pomalow.gaming@gmail.com>
2023-05-17 11:03:15 +0200

    substr upgrade

--- ft_substr.c
+++ ft_substr.c
@@ -3,18 +3,25 @@
 char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
     size_t i;
-    size_t end;
     char *sub;
 
-    i = start;
-    end = len + start;
-    sub = (char *) malloc(len * sizeof(char));
+    i = 0;
+    if(!s)
+        return(NULL);
+    if(start >= ft_strlen(s))
+        return((char *)ft_calloc(1, sizeof(char)));
+    if(ft_strlen(s) <= start + len)
+        sub = malloc(sizeof * sub * (ft_strlen(s) - start + 1));
+    else
+        sub = malloc(sizeof * sub * (len + 1));
     if(sub == NULL)
         return(NULL);
-    while(i < end)
+    while(s[start] && i < len)
     {
         sub[i] = s[i];
         i++;
+        start++;
     }
+    sub[i] = '\0';
     return(sub);
 }
