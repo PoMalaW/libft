4caf32e3729cf9ea08046b05c85caca32730a378
PoMalaW <pomalow.gaming@gmail.com>
2023-05-18 05:38:24 +0200

    update strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -2,16 +2,21 @@
 
 char    *ft_strdup(const char *s)
 {
-    const void *src;
-    void *dest;
-    size_t n;
+    char *dest;
+    size_t i;
+    
+    i = 0;
 
-    n = ft_strlen(s);
-    n++;
-    dest = (char *) malloc(n * sizeof(char));
-    if(dest == NULL)
+    if(!s)
         return(NULL);
-    src = (const void *)s;
-    ft_memcpy(dest, src, n);
+    dest = (char *) malloc((ft_strlen(s) + 1) * sizeof * dest);
+    if(!dest || !s)
+        return(NULL);
+    while (s[i] != '\0')
+    {
+        dest[i] = s[i];
+        i++;
+    }
+    dest[i] = '\0';
     return(dest);
 }
