23a73f27a9c5fbb7a89497004b28717a9eaa287e
PoMalaW <pomalow.gaming@gmail.com>
2023-05-25 11:17:38 +0200

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,26 +1,24 @@
 #include "libft.h"
 
-char    *ft_strnstr(const char *s1, const char *s2, size_t n)
+char    *ft_strnstr(const char *big, const char *little, size_t n)
 {
     size_t i;
     size_t j;
-    char *big;
-    char *small;
 
-    big = (char *)s1;
-    small = (char *)s2;
     i = 0;
-    j = 0;
-
-    if(s2 == NULL)
-        return (big);
-    while(s2[j] != '\0')
+    if(little[0] == '\0')
+        return((char *)big);
+    while(big[i] && i < n)
     {
-        if(s1[i] == s2[j])
+        j = 0;
+        if(big[i] == little[j])
         {
-            while(s1[i + j] == s2[j] && j < n)
+            while(big[i + j] == little[j] && (i + j) < n)
+            {
                 j++;
-            return(&small[i]);
+                if(little[j] == '\0')
+                    return((char *) &big[i]);
+            }
         }
         i++;
     }
