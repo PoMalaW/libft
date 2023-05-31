363de819a3b02a79c14392a7ae6d099af8e5cb51
PoMalaW <pomalow.gaming@gmail.com>
2023-05-31 21:12:39 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -1,71 +1,56 @@
 #include "libft.h"
 
-size_t  ft_len(char const *s, char c)
+size_t  ft_arrayLen(char const *s, char c)
 {
     size_t len;
     size_t i;
 
+    len = 0;
     i = 0;
-    while(s[i] != '\0')
-    {
-        if(!(s[i] == c))
-        {
-            len++;
-            i++;
-        }
-        else
-            i++;
-    }
-    return(len);
-}
-
-size_t  ft_count(char const *s, char c)
-{
-    size_t count;
-    size_t i;
-
-    while(s[i] != '\0')
+    while(s[i])
     {
         if(s[i] == c)
         {
-            i++;
-            count++;
+            while(s[i] && s[i] == c)
+                i++;
         }
         else
-            i++;
+        {
+            len++;
+            while(s[i] && s[i] != c)
+                i++;
+        }
     }
-    return(count);
+    return len;
 }
 
 
 char    **ft_split(char const *s, char c)
 {
-    char **array;
-    size_t len;
-    size_t count;
-    size_t i;
-    size_t j;
-    size_t k;
+    char    **array;
+    size_t  i;
+    size_t  j;
+    size_t  start;
 
     i = 0;
     j = 0;
-    k = 0;
-    len = ft_len(s, c);
-    count = ft_count(s, c);
-    array = malloc((len + count) * sizeof (array));
-    while(s[i] != '\0')
+    start = 0;
+    if(!s)
+        return NULL;
+    array = malloc((ft_arrayLen(s,c) + 1) * sizeof(char *));
+    if(array == NULL)
+        return NULL;
+    while(s[i])
     {
-        while(s[i] != c)
-        {
-            array[k][j] = s[i];
+        while(s[i] == c)
             i++;
-            j++;
-        }
-        array[k][j] = '\0';
-        j = 0;
-        i++;
-        k++;
+        if(s[i] == '\0')
+            break;
+        start = i;
+        while(s[i] && s[i] != c)
+            ++i;
+        array[j++] = ft_substr(s,start, i - start);
     }
-    array[k] = 0;
-    return(array);
+    array[j] = NULL;
+    return array;
 }
